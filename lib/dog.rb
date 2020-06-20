7d951212b5b88e71405f7b2d511d46fa064e752c
Alice <agenkin1@gmail.com>
2020-06-20 18:35:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,9 @@
 class Dog
-  def name(dog_name)
-    puts name
-    returns dog_name
+  def name
+    @this_dogs_name
   end
   def name=(dog_name)
-    puts dog_name
+    @this_dogs_name = dog_name
   end
 
   def bark
